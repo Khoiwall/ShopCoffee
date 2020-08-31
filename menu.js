6e9cd9ea259c4a8a66748eb218809602a9c5c488
Your name here <khoitr190@gmail.com>
2020-08-31 23:28:46 +0700

    project 3 front-end

--- menu.js
+++ menu.js
@@ -2,7 +2,9 @@ Vue.component('menu-mobile',{
     template:`
         <div class="hidden-menu" id = "site-menu-handle">
             <div class="content-menu">
-
+                <div class="close-menu" id="close-menu">
+                    <span style="font-size: 20px"><i class="fas fa-times-circle"></i></span>
+                </div>
             </div>
         </div>
     `
@@ -14,12 +16,20 @@ var menu = new Vue({
 
 var clickMenu = document.getElementById('click-menu')
 var siteMenuHandle = document.getElementById('site-menu-handle')
+var shadow = document.getElementById('shadow')
+var shadowNav = document.getElementById('shadow-nav')
+var closeMenu = document.getElementById('close-menu')
 
 clickMenu.addEventListener('click',function(){
-    if(siteMenuHandle.className === 'hidden-menu'){
-        siteMenuHandle.className += ' appear-menu'
-    }
-    else{
-        siteMenuHandle.className = 'hidden-menu'
-    }
+    siteMenuHandle.className += ' appear-menu'
+    shadow.className += ' appear-shadow'
+    shadowNav.style.backgroundColor = 'rgba(110, 108, 108, 0.8)'
+    clickMenu.style.display = "none"
+})
+
+closeMenu.addEventListener('click',function(){
+    siteMenuHandle.className = 'hidden-menu'
+    shadow.className = 'shadow-of-menu hidden'
+    shadowNav.style.backgroundColor = 'rgb(255, 255, 255)'
+    clickMenu.style.display = "block"
 })

037ed7ae9fc8ee9c7800fffe63dd2763ac29e867
Your name here <khoitr190@gmail.com>
2020-09-02 00:40:54 +0700

    inprove menu mobile

--- menu.js
+++ menu.js
@@ -5,9 +5,37 @@ Vue.component('menu-mobile',{
                 <div class="close-menu" id="close-menu">
                     <span style="font-size: 20px"><i class="fas fa-times-circle"></i></span>
                 </div>
+                <div class="style-list-menu">
+                    <div class="list-menu-mobile">
+                        <ul>
+                            <li v-for="(value,index) in menuList">{{ value.list }}
+                                <span class="arrow-right" v-bind:id="clickMenu(index)">
+                                    <i class="fas fa-angle-double-right"></i>
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
-    `
+    `,
+    data(){
+        return{
+            menuList: [
+                {list: 'HOME'},
+                {list: 'PAGE'},
+                {list: 'PORTFOLIO'},
+                {list: 'BLOG'},
+                {list: 'SHOP'},
+                {list: 'CONTACT'}
+            ]
+        }
+    },
+    methods:{
+        clickMenu: function(k){
+            return 'clickMenuFaTher' + k.toString();
+        }
+    }
 })
 
 var menu = new Vue({

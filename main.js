673fae40f569848a9956cdf9f0f3ac9238eaf416
Your name here <khoitr190@gmail.com>
2020-09-03 16:08:02 +0700

    improve menu mobile

--- main.js
+++ main.js
@@ -19,47 +19,16 @@ Vue.component('nav-bar',{
                 <ul class="menu-of-portfolio">
                     <li v-for="(portfolio,key) in portfolios" v-bind:class="appearPortfolio(key)">
                         <a href="#">{{portfolio.list.toLocaleUpperCase()}}</a>
-                        <span class="arrowRight" v-if="number(key) === 3">
-                            <i class="fas fa-arrow-right"></i>
-                        </span>
-                        <span class="arrowRight-1" v-else-if="number(key) === 4">
-                            <i class="fas fa-arrow-right"></i>
-                        </span>
-                        <ul v-if="number(key) === 3" class="menu-children-of-portfolio">
-                            <li v-for="column in columns"><a href="#">{{column.list.toLocaleUpperCase()}}</a></li>
-                        </ul>
-                        <ul v-if="number(key) === 4" class="menu-children-of-portfolios">
-                            <li v-for="columnO in columnOs"><a href="#">{{columnO.list.toLocaleUpperCase()}}</a></li>
-                        </ul>
                     </li>
                 </ul>
             </li>
             <li><a href="#">BLOG</a>
                 <ul class="menu-of-blog">
                     <li v-for="(value,key) in blogs" v-bind:class="appearBlogs(key)"><a href="#">{{value.list.toLocaleUpperCase()}}</a>
-                        <span class="arrowRight-2" v-if="number(key) === 3">
-                            <i class="fas fa-arrow-right"></i>
-                        </span>
-                        <ul class="menu-children-of-blog" v-if="number(key) === 3">
-                            <li v-for="value in childBlogs"><a href="#">{{value.list.toLocaleUpperCase()}}</a></li>
-                        </ul>
-                    </li>
                  </ul>
             </li>
-            <li><a href="#">SHOP</a>
-                <ul class="menu-of-shop">
-                    <li v-for="(value,key) in shops" v-bind:class="appearShops(key)"><a>{{value.list.toLocaleUpperCase()}}</a>
-                        <span class="arrowRight-3" v-if="number(key) === 3">
-                            <i class="fas fa-arrow-right"></i>
-                        </span>
-                        <ul class="menu-children-of-shop" v-if="number(key) === 3">
-                            <li v-for="value in childShops"><a href="#">{{value.list.toLocaleUpperCase()}}</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li><a href="#">CONTACT</a>
-            </li>
+            <li><a href="#">SHOP</a></li>
+            <li><a href="#">CONTACT</a></li>
         </ul>
 
 `,
@@ -77,57 +46,17 @@ data(){
             {list: 'Our Team'},
             {list: 'What We Offer'},
             {list: 'Our Menu'},
-            {list: 'Locations'},
-            {list: 'Contact US'}
+            {list: 'Locations'}
         ],
         portfolios:[
             {list: 'Standard'},
             {list: 'Gallery'},
-            {list: 'Masonry'},
-            {list: 'Layouts'},
-            {list: 'Single'}
-        ],
-        columns:[
-            {list: 'Two Columns'},
-            {list: 'Three Columns'},
-            {list: 'Three Columns Wide'},
-            {list: 'Four Columns'},
-            {list: 'Four Columns Wide'},
-            {list: 'Five Columns Wide'}
-        ],
-        columnOs:[
-            {list: 'Small Images'},
-            {list: 'Small slider'},
-            {list: 'Big Images'},
-            {list: 'Big Slider'},
-            {list: 'Small Gallery'},
-            {list: 'Big Gallery'}
-            
+            {list: 'Masonry'}
         ],
         blogs:[
             {list: 'Right Sidebar'},
             {list: 'Left Sidebar'},
-            {list: 'Without Sidebar'},
-            {list: 'Post Types'}
-        ],
-        childBlogs:[
-            {list: 'Standard'},
-            {list: 'Gallery'},
-            {list: 'Link'},
-            {list: 'Quote'},
-            {list: 'Video'},
-            {list: 'Audio'}
-        ],
-        shops:[
-            {list: 'Product List'},
-            {list: 'Product Single'},
-            {list: 'Shop Layouts'},
-            {list: 'Shop Pages'}
-        ],
-        childShops:[
-            {list: 'My Account'},
-            {list: 'Cart'},
-            {list: 'Checkout'}
+            {list: 'Without Sidebar'}
         ]
     }
 },
--- menu.js
+++ menu.js
@@ -8,10 +8,14 @@ Vue.component('menu-mobile',{
                 <div class="style-list-menu">
                     <div class="list-menu-mobile">
                         <ul>
-                            <li v-for="(value,index) in menuList">{{ value.list }}
-                                <span class="arrow-right" v-bind:id="clickMenu(index)">
+                            <li v-for="(value,index) in menuList" style="z-index: 300; background-color: #fff;"><a href="#" v-bind:id="clickMenu(index)">{{ value.list }}</a>
+                                <span class="border-bottom"></span>
+                                <span class="arrow-right" v-bind:class="hiddenArrow(index)">
                                     <i class="fas fa-angle-double-right"></i>
                                 </span>
+                                <ul class = "menu-mobile-children" v-bind:id="clickMenuChilderen(index)" style="z-index: 200;">
+                                    <li v-for="valueChildren in menuChildren(index)"><a href="#">{{ valueChildren.list }}</a></li>
+                                </ul>
                             </li>
                         </ul>
                     </div>
@@ -28,14 +32,64 @@ Vue.component('menu-mobile',{
                 {list: 'BLOG'},
                 {list: 'SHOP'},
                 {list: 'CONTACT'}
+            ],
+            homes:[
+                {list: 'Mocha'},
+                {list: 'Espresso'},
+                {list: 'Robusta'},
+                {list: 'Landing'}
+            ],
+            pages:[
+                {list: 'About US'},
+                {list: 'About Me'},
+                {list: 'Our Team'},
+                {list: 'What We Offer'},
+                {list: 'Our Menu'},
+                {list: 'Locations'}
+            ],
+            portfolios:[
+                {list: 'Standard'},
+                {list: 'Gallery'},
+                {list: 'Masonry'}
+            ],
+            blogs:[
+                {list: 'Right Sidebar'},
+                {list: 'Left Sidebar'},
+                {list: 'Without Sidebar'}
             ]
         }
     },
     methods:{
         clickMenu: function(k){
             return 'clickMenuFaTher' + k.toString();
+        },
+        hiddenArrow: function(k){
+            if (k === 4 || k === 5){
+                return 'hidden-arrow'
+            }
+            return ''
+        },
+        menuChildren(k){
+            if (k === 0){
+                return this.homes
+            }
+            else if(k === 1){
+                return this.pages
+            }
+            else if(k === 2){
+                return this.portfolios
+            }
+            else if(k === 3){
+                return this.blogs
+            }
+            else{
+                return []
+            }
+        },
+        clickMenuChilderen(k){
+            return 'appear-menu-children' + k.toString()
         }
-    }
+    },
 })
 
 var menu = new Vue({
@@ -60,4 +114,46 @@ closeMenu.addEventListener('click',function(){
     shadow.className = 'shadow-of-menu hidden'
     shadowNav.style.backgroundColor = 'rgb(255, 255, 255)'
     clickMenu.style.display = "block"
+})
+
+var clickMenuFaTher0 = document.getElementById('clickMenuFaTher0');
+var appearMenuChildren0 = document.getElementById('appear-menu-children0')
+var clickMenuFaTher1 = document.getElementById('clickMenuFaTher1');
+var appearMenuChildren1 = document.getElementById('appear-menu-children1')
+var clickMenuFaTher2 = document.getElementById('clickMenuFaTher2');
+var appearMenuChildren2 = document.getElementById('appear-menu-children2')
+var clickMenuFaTher3 = document.getElementById('clickMenuFaTher3');
+var appearMenuChildren3 = document.getElementById('appear-menu-children3')
+
+clickMenuFaTher0.addEventListener('click',function(){
+    if (appearMenuChildren0.className === 'menu-mobile-children'){
+        appearMenuChildren0.className += ' abc'
+    }
+    else{
+        appearMenuChildren0.className = 'menu-mobile-children'
+    }
+})
+clickMenuFaTher1.addEventListener('click',function(){
+    if (appearMenuChildren1.className === 'menu-mobile-children'){
+        appearMenuChildren1.className += ' abc'
+    }
+    else{
+        appearMenuChildren1.className = 'menu-mobile-children'
+    }
+})
+clickMenuFaTher2.addEventListener('click',function(){
+    if (appearMenuChildren2.className === 'menu-mobile-children'){
+        appearMenuChildren2.className += ' abcd'
+    }
+    else{
+        appearMenuChildren2.className = 'menu-mobile-children'
+    }
+})
+clickMenuFaTher3.addEventListener('click',function(){
+    if (appearMenuChildren3.className === 'menu-mobile-children'){
+        appearMenuChildren3.className += ' abcd'
+    }
+    else{
+        appearMenuChildren3.className = 'menu-mobile-children'
+    }
 })
